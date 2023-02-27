7db8751d129cea30948ee298e418aa414c1b5bb6
adleonard96 <alexanderleonard27@gmail.com>
2023-02-26 22:39:20 -0500

    Added 2 API Routes

--- src/main/java/com/mango/blog/Post/GeneralPost.java
+++ src/main/java/com/mango/blog/Post/GeneralPost.java
@@ -1,5 +1,6 @@
 package com.mango.blog.Post;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.mango.blog.Comment.Comment;
 
 import java.time.LocalDateTime;
@@ -7,18 +8,103 @@ import java.util.ArrayList;
 import java.util.UUID;
 
 public class GeneralPost implements Post{
-    public UUID postID = null;
+    public UUID postID = UUID.randomUUID();
     public ArrayList<Comment> comments;
-    public String author = null;
-    public String postName = null;
-    public String postBody = null;
+    public String author;
+    public String postName;
+    public String postBody;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     public LocalDateTime createdOn = LocalDateTime.now();
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     public LocalDateTime editedOn = LocalDateTime.now();
-    public String text = null;
-    public String genre = null;
+    public String text;
+    public String genre;
 
     public GeneralPost(String postName, String text, String author, String genre) {
+        this.postName = postName;
+        this.text = text;
+        this.author = author;
+        this.genre = genre;
         comments = new ArrayList<>();
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+                "postID:" + postID +
+                ", comments:" + comments +
+                ", author:'" + author + '\'' +
+                ", postName:'" + postName + '\'' +
+                ", postBody:'" + postBody + '\'' +
+                ", createdOn:" + createdOn +
+                ", editedOn:" + editedOn +
+                ", text:'" + text + '\'' +
+                ", genre:'" + genre + '\'' +
+                '}';
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public void setPostName(String postName) {
+        this.postName = postName;
+    }
+
+    public void setPostBody(String postBody) {
+        this.postBody = postBody;
+    }
+
+    public void setCreatedOn(LocalDateTime createdOn) {
+        this.createdOn = createdOn;
+    }
+
+    public void setEditedOn(LocalDateTime editedOn) {
+        this.editedOn = editedOn;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public UUID getPostID() {
+        return postID;
+    }
+
+    public ArrayList<Comment> getComments() {
+        return comments;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getPostName() {
+        return postName;
+    }
+
+    public String getPostBody() {
+        return postBody;
+    }
+
+    public LocalDateTime getCreatedOn() {
+        return createdOn;
+    }
+
+    public LocalDateTime getEditedOn() {
+        return editedOn;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public String getGenre() {
+        return genre;
     }
 
     @Override
