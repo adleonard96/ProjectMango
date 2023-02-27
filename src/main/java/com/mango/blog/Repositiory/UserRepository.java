601d8e9d7fe2441e03360758eb970f9f038417b3
adleonard96 <alexander.leonard@stu.laroche.edu>
2023-02-26 22:39:20 -0500

    Added 2 API Routes

--- src/main/java/com/mango/blog/Repositiory/UserRepository.java
+++ src/main/java/com/mango/blog/Repositiory/UserRepository.java
@@ -2,6 +2,12 @@ package com.mango.blog.Repositiory;
 
 import com.mango.blog.User.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 public interface UserRepository extends MongoRepository<User, String>{
+    @Query(value = "{ 'userName' : ?0 }")
+    User findByUserName(String userName);
+
+
+    User findPostByUserName(String userName);
 }
