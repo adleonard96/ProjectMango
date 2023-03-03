b1aa3a11d3c2d8598030e286738999aec0c2cdc9
adleonard96 <alexander.leonard@stu.laroche.edu>
2023-03-02 22:09:11 -0500

    Added API to get a post by Id

--- src/main/java/com/mango/blog/Post/Post.java
+++ src/main/java/com/mango/blog/Post/Post.java
@@ -6,6 +6,7 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.UUID;
 
+
 public interface Post {
     public ArrayList<Comment> comments = new ArrayList<>();
     public LocalDateTime createdOn = LocalDateTime.now();
