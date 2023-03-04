f195a90bd363ca30c1df0b81a866e8edbf72e2b9
adleonard96 <alexander.leonard@stu.laroche.edu>
2023-03-04 18:27:39 -0500

    Added validations

--- src/main/java/com/mango/blog/Post/PostController.java
+++ src/main/java/com/mango/blog/Post/PostController.java
@@ -6,12 +6,14 @@ import com.mongodb.client.model.Accumulators;
 import com.mongodb.client.model.Aggregates;
 import com.mongodb.client.model.Filters;
 import com.mongodb.client.model.Projections;
+import jakarta.validation.Valid;
 import org.bson.Document;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.mango.blog.Repositiory.UserRepository;
 import com.mango.blog.User.User;
 import org.bson.json.JsonObject;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
