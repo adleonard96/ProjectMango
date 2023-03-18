e270464a7f9eac86d3d6fb5bf2096cb6988cad67
adleonard96 <alexanderleonard27@gmail.com>
2023-03-18 15:55:22 -0400

    Refactored APIs to have some error checking and changed all returns to ResponseEntity

--- src/main/java/com/mango/blog/Post/PostController.java
+++ src/main/java/com/mango/blog/Post/PostController.java
@@ -34,43 +34,57 @@ public class PostController {
 
     }
 
-    // TODO - Get the username from the current signed in user instead of the body
     @PostMapping("Posts/GeneralPost")
-    public ResponseEntity createGenericPostPost(@Valid @RequestBody GeneralPost post){
+    public ResponseEntity<String> createGenericPostPost(@Valid @RequestBody GeneralPost post){
         // Get the user from the database
         User user = repo.findByUserName(post.getAuthor());
+        if (user == null){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("User not found");
+        }
         user.createPost(post.getPostName(), post.getText(), post.getGenre(), post.getAuthor());
         // Save the user to the database
         repo.save(user);
         return ResponseEntity.status(HttpStatus.CREATED).body("Post Created");
     }
     @DeleteMapping("/GeneralPost")
-    public String deleteGenericPostPost (@RequestBody GeneralPost post){
+    public ResponseEntity<String> deleteGenericPostPost (@RequestBody GeneralPost post){
         User user = repo.findByUserName(post.getAuthor()); //Finding the user
+        if (user == null){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("User not found");
+        }
         user.deletePost(post.getPostID(),post.getPostName(), post.getText(), post.getGenre(), post.getAuthor()); //sending the post to the chopping block
         repo.save(user); //save new user
-        return "Post Deleted";
+        return ResponseEntity.status(HttpStatus.CREATED).body("Post Deleted");
     }
 
     @GetMapping("/Posts/GeneralPost")
-    public String getGenericPost(){
+    public ResponseEntity<String> getGenericPost(){
         User user = repo.findByUserName("Test");
+        if (user == null){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("User not found");
+        }
         System.out.println(user.getUserName());
         // Get the first post from the user
         Post post = user.getPosts().get(0);
+        if (post == null){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Post not found");
+        }
         ObjectMapper mapper = new ObjectMapper();
         mapper.registerModule(new JavaTimeModule());
         try{
             String json = mapper.writeValueAsString(post);
-            return json;
+            return ResponseEntity.status(HttpStatus.OK).body(json);
         }catch (Exception e){
             System.out.println(e);
-            return "Error";
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error");
         }
     }
 
     @GetMapping("/Posts/PostById")
-    public String postById(@RequestParam String postID){
+    public ResponseEntity<String> postById(@RequestParam String postID){
+        if (postID == null){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Post not found");
+        }
         MongoClient mongoClient = MongoClients.create("mongodb+srv://MangoAdmin:TdINg8HrP5HLNLJU@projectmango.34hfodq.mongodb.net/?retryWrites=true&w=majority");
         MongoDatabase database = mongoClient.getDatabase("MangoDB");
         MongoCollection<Document> collection = database.getCollection("BlogData");
@@ -82,35 +96,59 @@ public class PostController {
                         Aggregates.match(Filters.eq("postID", postID))
                 )
         );
-        return posts.first().toJson();
+        String json = posts.first().toJson();
+        return ResponseEntity.status(HttpStatus.OK).body(json);
     }
     
     public String PostById(@RequestParam String postID){
-
         return repo.getPostsById(postID);
     }
 
     @GetMapping("/Posts/Genres")
-    public String getGenres(){
-        return repo.getGenres();
+    public ResponseEntity<String> getGenres(){
+        String body = repo.getGenres();
+        return ResponseEntity.status(HttpStatus.OK).body(body);
     }
 
     @GetMapping("/Posts/ByGenre")
-    public String PostsByGenre(@RequestParam String genre){
-        return repo.getPostsByGenre(genre);
+    public ResponseEntity<String> PostsByGenre(@RequestParam String genre){
+
+        String body = repo.getPostsByGenre(genre);
+        return ResponseEntity.status(HttpStatus.OK).body(body);
+
     }
 
     @PutMapping("/Posts/UpdateGeneralPost")
-    public String updatePost(@RequestBody GeneralPost post){
+    public ResponseEntity<String> updatePost(@RequestBody GeneralPost post){
+        if (post.getAuthor() == null){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Author not found");
+        }
+        if (post.getPostID() == null){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Post not found");
+        }
+        if (post.getPostName() == null){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Post name not found");
+        }
+        if (post.getText() == null){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Post text not found");
+        }
+        if (post.getGenre() == null) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Post genre not found");
+        }
         User user = repo.findByUserName(post.getAuthor());
+        if (user == null){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("User not found");
+        }
+
         user.updatePost(post.getPostID(), post.getPostName(), post.getText(), post.getGenre(), post.getAuthor());
         repo.save(user);
-        return "Post Updated";
+        return ResponseEntity.status(HttpStatus.OK).body("Post Updated");
 
     }
 
     @GetMapping("/Posts/AllPosts")
-    public ArrayList<String> allPosts(){
-        return repo.getAllPosts();
+    public ResponseEntity<String> allPosts(){
+        String response = repo.getAllPosts().toString();
+        return ResponseEntity.status(HttpStatus.OK).body(response);
     }
 }
