4dbed7050713a4be04c3ee342eaa51d523ba0346
adleonard96 <alexander.leonard@stu.laroche.edu>
2023-03-27 17:22:34 -0400

    Added implementation of JWT to login

---
+++ src/main/java/com/mango/blog/Authentication/JwtGenerator.java
@@ -0,0 +1,34 @@
+package com.mango.blog.Authentication;
+
+import com.mango.blog.User.User;
+import io.jsonwebtoken.JwtBuilder;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.spec.SecretKeySpec;
+import java.security.Key;
+import java.util.Base64;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+@Service
+public class JwtGenerator implements JwtGeneratorInterface{
+
+    private final String SECRET = "Zr4u7x!A%C*F-JaNdRgUkXp2s5v8y/B?";
+
+    private String message = "Token generated successfully";
+    @Override
+    public Map<String, String> generatetoken(User user) {
+        String jwtToken="";
+        SignatureAlgorithm signatureAlgorithm = SignatureAlgorithm.HS256;
+        byte[] secretBytes = Base64.getEncoder().encode(SECRET.getBytes());
+        Key signingKey = new SecretKeySpec(secretBytes, signatureAlgorithm.getJcaName());
+        JwtBuilder builder = Jwts.builder().setSubject(user.getUserName()).setIssuedAt(new Date()).signWith(SignatureAlgorithm.HS256, signingKey);
+        jwtToken = builder.compact();
+        Map<String, String> jwtTokenGen = new HashMap<>();
+        jwtTokenGen.put("token", jwtToken);
+        jwtTokenGen.put("message", message);
+        return jwtTokenGen;
+    }
+}
